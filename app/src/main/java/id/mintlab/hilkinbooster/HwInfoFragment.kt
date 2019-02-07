621be386aa05fe1a11826a6c59b9d85c2b52a81e
agung <agungofficial@gmail.com>
2019-02-08 02:03:11 +0800

    hardware info lol

--- app/src/main/java/id/mintlab/hilkinbooster/HwInfoFragment.kt
+++ app/src/main/java/id/mintlab/hilkinbooster/HwInfoFragment.kt
@@ -1,27 +1,36 @@
 package id.mintlab.hilkinbooster
 
 import android.graphics.Color
+import android.graphics.Point
 import android.os.Bundle
+import android.os.CpuUsageInfo
+import android.os.Handler
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.hookedonplay.decoviewlib.DecoView
-import com.hookedonplay.decoviewlib.charts.SeriesItem
-import com.hookedonplay.decoviewlib.events.DecoEvent
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import kotlin.random.Random
+import android.app.ActivityManager
+import android.content.Context.ACTIVITY_SERVICE
 
-class HwInfoFragment : Fragment() {
 
-    var cpuChart : DecoView? = null
-    var ramChart : DecoView? = null
-    var storageChart : DecoView? = null
+open class HwInfoFragment : Fragment() {
+
+
+    var lineChart : LineChart? = null
+    var index : Float = 0f
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         val view = inflater.inflate(R.layout.fragment_hwinfo, container, false)
-        cpuChart = view.findViewById<DecoView>(R.id.cpuChart)
-        ramChart = view.findViewById<DecoView>(R.id.ramChart)
-        storageChart = view.findViewById<DecoView>(R.id.storageChart)
+
+        lineChart = view.findViewById<LineChart>(R.id.chart)
 
         return view
     }
@@ -29,32 +38,50 @@ class HwInfoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //Create data series track
-        val cpuSeries = SeriesItem.Builder(Color.argb(255, 255, 0, 0), Color.argb(255, 64, 255, 64))
-            .setRange(0f, 100f, 0f)
-            .setLineWidth(32f)
-            .setChartStyle(SeriesItem.ChartStyle.STYLE_LINE_HORIZONTAL)
-            .build()
-
-        val ramSeries = SeriesItem.Builder(Color.argb(255, 255, 0, 0), Color.argb(255, 64, 255, 64))
-            .setRange(0f, 100f, 0f)
-            .setLineWidth(32f)
-            .setChartStyle(SeriesItem.ChartStyle.STYLE_LINE_VERTICAL)
-            .build()
-
-        val storageSeries = SeriesItem.Builder(Color.argb(255, 255, 0, 0), Color.argb(255, 64, 255, 64))
-            .setRange(0f, 100f, 0f)
-            .setLineWidth(32f)
-            .setChartStyle(SeriesItem.ChartStyle.STYLE_PIE)
-            .build()
-
-        val cpuIndex = cpuChart?.addSeries(cpuSeries)!!
-        val ramIndex = ramChart?.addSeries(ramSeries)!!
-        val storageIndex = storageChart?.addSeries(storageSeries)!!
-
-        cpuChart?.addEvent(DecoEvent.Builder(25f).setIndex(cpuIndex).build())
-        ramChart?.addEvent(DecoEvent.Builder(80f).setIndex(ramIndex).build())
-        storageChart?.addEvent(DecoEvent.Builder(100f).setIndex(storageIndex).build())
+        updateData()
 
     }
+
+    @Synchronized fun updateData(){
+        val entries = ArrayList<Entry>()
+        var lineData : LineData
+        lineChart?.xAxis?.textColor = Color.TRANSPARENT
+        lineChart?.axisLeft?.textColor = Color.TRANSPARENT
+        lineChart?.axisRight?.textColor = Color.TRANSPARENT
+        lineChart?.legend?.isEnabled = false
+        lineChart?.setBorderWidth(3f)
+        val description = Description()
+        description.text = ""
+        description.textColor = resources.getColor(R.color.colorPrimary)
+        lineChart?.description = description
+
+
+
+        Thread(
+            Runnable {
+                while (true){
+                    entries.add(Entry(index, Random.nextInt(10).toFloat()))
+                    lineData = LineData(LineDataSet(entries, ""))
+                    lineData.setValueTextColor(Color.GRAY)
+                    lineChart?.data = lineData
+
+                    lineChart?.setVisibleXRange(0f, 10f)
+                    lineChart?.moveViewToX(index)
+                    index++
+                    info()
+                    Thread.sleep(1000)
+                }
+            }).start()
+    }
+
+    fun info(){
+        val activityMgr = activity?.getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        val memoryInfo = ActivityManager.MemoryInfo()
+        activityMgr.getMemoryInfo(memoryInfo)
+
+        Log.d("Aval memory :", memoryInfo.availMem.toString())
+        Log.d("low memory :", memoryInfo.lowMemory.toString())
+        Log.d("threshold memory :", memoryInfo.threshold.toString())
+    }
+
 }
